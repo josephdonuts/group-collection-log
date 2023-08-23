9fe5e8672f05bb9cd19d45af735030e82318bf26
Joey Muller <joeemuller@gmail.com>
2023-08-23 16:10:53 -0400

    format

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -5,10 +5,10 @@ const Item = (props) => {
     const { id, name, obtained, quantity } = props.item;
     
     return (
-            <a className="item-tile" href={`https://oldschool.runescape.wiki/w/${name.replace(/ /g, '_')}`} >
-                <span className="item-quantity-text">{quantity}</span>
-                <img className="item-icon" src={`data:image/jpeg;charset=utf-8;base64,${icons[id]}`} alt={name} />
-            </a>
+        <a className="item-tile" href={`https://oldschool.runescape.wiki/w/${name.replace(/ /g, '_')}`} >
+            <span className="item-quantity-text">{quantity}</span>
+            <img className="item-icon" src={`data:image/jpeg;charset=utf-8;base64,${icons[id]}`} alt={name} />
+        </a>
     )
 }
 
