95085f4cc518db3d0708d5c355be7908ce7aaa5b
Joey Muller <joeemuller@gmail.com>
2023-08-23 12:42:03 -0400

    added countUniqueItems to groupedLog

--- server/src/LogCombiner.js
+++ server/src/LogCombiner.js
@@ -7,7 +7,7 @@ class LogCombiner {
 
     combine() {
         this.groupedLog = LogCombiner.sumObjectsArray(this.logs);
-        //this.uniqueItems = LogCombiner.countUniqueItems(this.groupedLog);
+        this.uniqueItems = LogCombiner.countUniqueItems(this.groupedLog);
     }
 
     static sumObjects = (obj1, obj2) => {
@@ -18,7 +18,7 @@ class LogCombiner {
                         obj1[key] = [...obj2[key]];
                     } else {
                         for (let i = 0; i < obj2[key].length; i++) {
-                            if (typeof obj2[key][i] === 'object' && obj2[key][i] !== null) {
+                            if (typeof obj2[key][i] === "object" && obj2[key][i] !== null) {
                                 if (!obj1[key][i]) {
                                     obj1[key][i] = {};
                                 }
@@ -26,17 +26,23 @@ class LogCombiner {
                             }
                         }
                     }
-                } 
+                }
                 // ^^^ don't change arrays to objects ^^^
                 //else if its not an array...
-                else if (typeof obj2[key] === 'number') {
-                    if (!(key === 'id' || key === 'uniqueObtained' || key === 'uniqueItems')) { //dont sum these
+                else if (typeof obj2[key] === "number") {
+                    if (!(key === "id" || key === "uniqueObtained" || key === "uniqueItems")) {
+                        //dont sum these
                         if (!obj1[key]) {
                             obj1[key] = 0;
                         }
                         obj1[key] += obj2[key];
                     }
-                } else if (typeof obj2[key] === 'object' && obj2[key] !== null) {
+                } else if (typeof obj2[key] === "boolean" && key === "obtained") {
+                    if (!obj1[key]) {
+                        obj1[key] = false;
+                    }
+                    obj1[key] = obj1[key] || obj2[key];
+                } else if (typeof obj2[key] === "object" && obj2[key] !== null) {
                     if (!obj1[key]) {
                         obj1[key] = {};
                     }
@@ -48,7 +54,7 @@ class LogCombiner {
                 }
             }
         }
-    }
+    };
 
     static sumObjectsArray = (objectsArray) => {
         let result = {};
@@ -56,19 +62,28 @@ class LogCombiner {
             LogCombiner.sumObjects(result, obj);
         }
         return result;
-    }
+    };
+    static countUniqueItems(groupedLog) {
+        let count = 0;
 
-    static countUniqueItems = (groupedLog) => {
-        let uniqueItems = 0;
-        for (let key in groupedLog) {
-            if (groupedLog.hasOwnProperty(key)) {
-                if (typeof groupedLog[key] === 'object' && groupedLog[key] !== null) {
-                    uniqueItems += LogCombiner.countUniqueItems(groupedLog[key]);
-                } else if (key === 'obtained' && groupedLog[key] === true) {
-                    uniqueItems++;
+        // Recursive helper function to handle arrays of objects
+        const traverseObject = (obj) => {
+            for (let key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (key === "obtained" && obj[key] === true) {
+                        count++;
+                    } else if (Array.isArray(obj[key])) {
+                        obj[key].forEach((item) => {
+                            traverseObject(item);
+                        });
+                    } else if (typeof obj[key] === "object" && obj[key] !== null) {
+                        traverseObject(obj[key]);
+                    }
                 }
             }
-        }
+        };
+        traverseObject(groupedLog);
+        return count;
     }
 }
 
