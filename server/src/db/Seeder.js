0197267df9165a1c1f2ea8df44d7d56856439a5d
Joey Muller <joeemuller@gmail.com>
2023-08-25 13:50:38 -0400

    added hiscores seeder

--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 import { connection } from "../boot.js"
+import Hiscore from "../models/Hiscore.js"
 
 class Seeder {
   static async seed() {
     // include individual seed commands here
-
+    console.log("seeding hiscores")
+    await Hiscore.seed()
     console.log("Done!")
     await connection.destroy()
   }
--- server/src/models/Hiscore.js
+++ server/src/models/Hiscore.js
@@ -4,6 +4,17 @@ class Hiscore extends Model {
     static get tableName() {
         return 'hiscores'
     }
+    static async seed() {
+        await Hiscore.query().insert([
+            {id: '7', groupName: 'propanemoney', uniques: 802, prestige: false },
+            {id: '4', groupName: 'dad squad', uniques: 729, prestige: false },
+            {id: '1', groupName: 'ratscape', uniques: 627, prestige: true },
+            {id: '5', groupName: 'boobie bois', uniques: 550, prestige: false },
+            {id: '2', groupName: 'chile', uniques: 119, prestige: false },
+            {id: '3', groupName: 'sub2', uniques: 15, prestige: true},
+            {id: '6', groupName: 'corporeal', uniques: 0, prestige: true},
+        ])
+    }
 }
 
 module.exports = Hiscore
